6df7e782686cf1d83cc41bc4a6d9cef4f8798245
Dai Kawahara <skbsporta7@gmail.com>
2021-08-11 10:13:11 +0900

    update template and add sample file

---
+++ Xcode_template_sample/Dog.swift
@@ -0,0 +1,12 @@
+//
+//  Dog.swift
+//  Xcode_template_sample
+//
+//  Created by kawaharadai on 2021/08/11.
+//
+
+final class Dog: Animal {
+    let name = "tama"
+    let age = 10
+    let weight = 30
+}
---
+++ tooling/Animal.xctemplate/___FILEBASENAME___Animal.swift
@@ -0,0 +1,7 @@
+//___FILEHEADER___
+
+final class ___VARIABLE_className___: Animal {
+    let name = "___VARIABLE_animalName___"
+    let age = ___VARIABLE_animalAge___
+    let weight = ___VARIABLE_animalWeight___
+}
